2d055acc04fe5772f23b6c2d3aab0e2cc9f7e11b
hawyeet <53439203+hawyeet@users.noreply.github.com>
2025-01-03 22:22:12 +0700

    Fixed popup not reverting on mobile.

--- card-portfolio/src/components/Card.tsx
+++ card-portfolio/src/components/Card.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/Card.css";
 
 interface CardProps {
   cover: string;
-  // title: string;
   character: string;
 }
 
 const Card: React.FC<CardProps> = ({ cover, character }) => {
   const [isActive, setIsActive] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
 
-  const handleClick = () => {
+  const handleClick = (event: MouseEvent) => {
+    if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+      setIsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  const handleCardClick = () => {
     setIsActive((prev) => !prev);
   };
 
@@ -18,13 +31,13 @@ const Card: React.FC<CardProps> = ({ cover, character }) => {
     <div>
       <a href="#">
         <div
+          ref={cardRef}
           className={`card ${isActive ? "active" : ""}`}
-          onClick={handleClick}
+          onClick={handleCardClick}
         >
           <div className="wrapper">
             <img src={cover} className="cover-image" />
           </div>
-          {/* <img src={title} className="title" /> */}
           <img src={character} className="character" />
         </div>
       </a>
