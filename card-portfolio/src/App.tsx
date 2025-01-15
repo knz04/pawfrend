3c5f7430247328e262adba3f2f4fd477c27684bf
hawyeet <53439203+knz04@users.noreply.github.com>
2025-01-15 11:04:52 +0700

    Added credits

--- card-portfolio/src/App.tsx
+++ card-portfolio/src/App.tsx
@@ -3,11 +3,16 @@ import Card from "./components/Card";
 import COVER from "./assets/background1.webp";
 import DECOR from "./assets/decor.webp";
 import CHARACTER from "./assets/popup.webp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="container">
-      <Card cover={COVER} character={CHARACTER} decor={DECOR} />
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow flex justify-center items-center">
+        <Card cover={COVER} character={CHARACTER} decor={DECOR} />
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ card-portfolio/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="mb-2 text-sm text-center text-[#cb9a66]">
+        Art by Gress | Assembled by Kanza
+      </p>
+    </footer>
+  );
+}
